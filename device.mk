44eb90ab34b256f901e078803b5abf3cba62fce2
ItsVixano <vixanonew@gmail.com>
2021-12-10 22:53:22 +0100

    ysl: Import audio configs
    * from V12.0.2.0.PEFMIXM
    
    Change-Id: I3790d2cc6111ec9d1fe5a50a280a58f873f80ed6

--- device.mk
+++ device.mk
@@ -18,6 +18,11 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1440
 TARGET_SCREEN_WIDTH := 720
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
